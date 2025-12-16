82a587fba87f1c577e5d8d5048c6df292d8d7876
Berry7028 <s.kouta7028@gmail.com>
2025-12-16 12:53:34 +0900

    Implement custom login view and update URL patterns; add cache control to logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,17 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 
-from .views import SignupView
+from .views import SignupView, CustomLoginView
 
 app_name = 'accounts'
 
+# ログアウトビューをデコレートする
+LogoutView = method_decorator(never_cache, name='dispatch')(auth_views.LogoutView)
+
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from django.views.generic.edit import CreateView
 
 from .forms import SignupForm
@@ -16,4 +19,10 @@ class SignupView(CreateView):
 
     def get_success_url(self):
         return reverse('posts:post_list')
+
+
+@method_decorator(never_cache, name='dispatch')
+class CustomLoginView(auth_views.LoginView):
+    """キャッシュを無効にするカスタムログインビュー"""
+    pass
 
